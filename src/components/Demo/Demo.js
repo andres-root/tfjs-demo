ee104dec0e3add7ceb02eae5a86f8aae78e92ae2
Andres Root <andreslujandev@gmail.com>
2020-06-10 01:50:04 -0500

    Init

---
+++ src/components/Demo/Demo.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import * as tf from '@tensorflow/tfjs-node'
+
+class Demo extends React.Component {
+    render() {
+      return <h1>Hello, {this.props.name}</h1>;
+    }
+}
+
+export default Demo;
